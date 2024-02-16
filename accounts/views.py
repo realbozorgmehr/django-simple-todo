49a0e5d0e6753d3348ee927dddee774d7e6bb6dc
bozorgmehr <abolfazlkameli0@gmail.com>
2024-02-16 18:26:20 +0330

    rewrote todo-update-view with simple cbv login required for logout now

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
 
@@ -10,6 +11,12 @@ from django.views import View
 class UserRegisterView(View):
     form_class = UserRegisterForm
     template_name = 'accounts/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         form = self.form_class()
@@ -20,29 +27,43 @@ class UserRegisterView(View):
         if form.is_valid():
             cd = form.cleaned_data
             User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
             messages.success(request, 'Account created successfully!', extra_tags='success')
             return redirect('home:home')
         return render(request, self.template_name, context={'form': form})
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, context={'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
                 login(request, user)
-                messages.success(request, 'user logged in successfully', 'success')
+                messages.success(request, 'logged in successfully!', extra_tags='success')
                 return redirect('home:home')
             else:
-                messages.error(request, 'usrename or password is invalid', 'danger')
-    else:
-        form = UserLoginForm()
-    return render(request, 'accounts/login.html', {'form': form})
+                messages.error(request, "username or password is wrong", extra_tags='danger')
+        return render(request, self.template_name, context={"form": form})
 
 
-class UserLogoutView(View):
+class UserLogoutView(LoginRequiredMixin, View):
     def get(self, request):
         logout(request)
         messages.success(request, 'logged out successfully', 'success')
