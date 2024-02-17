7fa96e5aa5fcc3096a4c5de5c7340df3af0eb249
bozorgmehr <abolfazlkameli0@gmail.com>
2024-02-17 23:32:02 +0330

    add slug and user selection for new todo(admin panel)

--- home/models.py
+++ home/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     title = models.CharField(max_length=250)
     body = models.TextField()
-    created = models.DateTimeField()
+    slug = models.SlugField(default=None)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.slug
