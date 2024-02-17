0047a610d7b6b9d3414990926a9361023cd4d71d
bozorgmehr <abolfazlkameli0@gmail.com>
2024-02-17 23:30:51 +0330

    add authenticate with email and a simple user profile

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Default Login url
 MEDIA_URL = '/accounts/login/'
+
+# Default Backend url
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.authenticate.EmailBackend'
+]
