c4fe5730ea7e23a2ea8668e228d9a7453f97b98a
bozorgmehr <abolfazlkameli0@gmail.com>
2024-02-22 17:33:18 +0330

    edited admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Todo
 
+
 # Register your models here.
-admin.site.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'updated', 'user')
+    list_filter = ('updated',)
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('user',)
+
+
+admin.site.register(Todo, TodoAdmin)
