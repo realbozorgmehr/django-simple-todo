f8e8d4a45b17e3078a31caa99480efd7c4aa6334
bozorgmehr <abolfazlkameli0@gmail.com>
2024-02-29 17:14:07 +0330

    add get absolute url

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
@@ -13,3 +14,6 @@ class Todo(models.Model):
 
     def __str__(self):
         return self.slug
+
+    def get_absolute_url(self):
+        return reverse('home:detail', args=(self.id, self.slug))
